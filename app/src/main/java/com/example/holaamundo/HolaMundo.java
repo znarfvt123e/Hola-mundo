40e42884dfc236e9cb545580ef7f7a5af9317ccd
franz villca <franzvtt@gmail.com>
2021-04-22 13:11:37 -0400

    Hola-mundo

---
+++ app/src/main/java/com/example/holaamundo/HolaMundo.java
@@ -0,0 +1,14 @@
+package com.example.holaamundo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HolaMundo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.hola_mundo);
+    }
+}
